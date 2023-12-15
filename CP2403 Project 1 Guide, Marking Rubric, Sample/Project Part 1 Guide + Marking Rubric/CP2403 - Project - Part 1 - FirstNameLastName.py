56697b37c0f4ddf642b38069714c9bfa77e8c1a2
ShweMoeThantAurum <128347903+ShweMoeThantAurum@users.noreply.github.com>
2023-12-15 22:06:58 +0800

    Add Info Processing Assignment 1
    The Past Submission

---
+++ CP2403 Project 1 Guide, Marking Rubric, Sample/Project Part 1 Guide + Marking Rubric/CP2403 - Project - Part 1 - FirstNameLastName.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # First Name:
+# # Last Name:
+
+# # Import Libraries  
+
+# In[ ]:
+
+
+
+
+
+# # Import Data
+
+# In[ ]:
+
+
+
+
+
+# # Data management 
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# # Box plot
+
+# In[ ]:
+
+
+
+
+
+# # Histogram 
+
+# In[ ]:
+
+
+
+
+
+# # Line chart 
+
+# In[ ]:
+
+
+
+
+
+# # Bubble chart 
+
+# In[ ]:
+
+
+
+
+
+# # Select Plot
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
